02e60bc30c32ca580f10550742969965884a049d
monikakkk <monika.konarska63@gmail.com>
2019-04-29 22:29:42 +0200

    define target and create variable "target"

--- 1.prepare_data.R
+++ 1.prepare_data.R
@@ -19,4 +19,9 @@ data <- fread(file.path(dataPath, "loan.csv"))
 data <- setDF(data)
 
 
+data$target <- factor(case_when(data$loan_status == "Fully Paid"  ~ 0,
+                                data$loan_status == "Charged Off" ~ 1,
+                                data$loan_status == "Late (31-120 days)" ~ 1,
+                                data$loan_status == "Default Does not meet the credit policy. Status:Charged Off" ~ 1), levels = c(0, 1))
+data <- data[which(!is.na(data$target)), ]
 
