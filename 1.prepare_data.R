c7c518f2a5492ad2b26769e11dff070cc23b8c66
monikakkk <monika.konarska63@gmail.com>
2019-06-19 07:47:55 +0200

    change steps to data cleaning and change assumptions
    funded loan date > 2013-01-01 and funded loan date 2015-06-01, first of all chose the time series of data

--- 1.prepare_data.R
+++ 1.prepare_data.R
@@ -14,32 +14,56 @@ dictionary <- read.xlsx(xlsxFile = file.path(dataPath, "LCDataDictionary.xlsx"),
 data <- fread(file.path(dataPath, "loan.csv"))
 data <- setDF(data)
 
+data <- data %>% mutate(funded_loan_date = convert_date_from_month_year(issue_d),
+                        earliest_cr_line_date = convert_date_from_month_year(earliest_cr_line),
+                        last_credit_pull_date = convert_date_from_month_year(last_credit_pull_d)) 
+
 data$target <- factor(case_when(data$loan_status == "Fully Paid"  ~ 0,
                                 data$loan_status == "Charged Off" ~ 1,
                                 data$loan_status == "Default Does not meet the credit policy. Status:Charged Off" ~ 1), levels = c(0, 1))
+
 data <- data[which(!is.na(data$target)), ]
 
-structureData <- describe_variables(data)
-levelOfNA <- 0.95
+
+target_plot_all_data <- data %>%
+  mutate(target = as.factor(as.numeric(as.character(target)))) %>%
+  select(c("target", "funded_loan_date")) %>%
+  group_by(funded_loan_date, target) %>%
+  summarise( N = n()) %>%
+  ggplot(aes(x = funded_loan_date, y = N, fill=target)) +
+  geom_bar(stat = 'identity', position= position_dodge())+
+  ylab("Number of observations")+
+  labs(title = "Target in all data")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+dataReduced <- data[which(data$funded_loan_date >= '2013-01-01' & data$funded_loan_date <= '2015-06-01'), ]
+
+target_plot_reduced_data <- dataReduced %>%
+  mutate(target = as.factor(as.numeric(as.character(target)))) %>%
+  select(c("target", "funded_loan_date")) %>%
+  group_by(funded_loan_date, target) %>%
+  summarise( N = n()) %>%
+  ggplot(aes(x = funded_loan_date, y = N, fill=target)) +
+  geom_bar(stat = 'identity', position= position_dodge())+
+  ylab("Number of observations")+
+  labs(title = "Target in all data")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+structureData <- describe_variables(dataReduced)
+levelOfNA <- 0.80
 maxUniqueValues <- 30
 
 variablesWithalotNA         <- structureData$variable[which(structureData$p_numberOfNa >= levelOfNA )]
 variablesWithalotUniqueText <- structureData$variable[ which(structureData$type == 'character' & structureData$uniqueValues > maxUniqueValues)]
-variablesWithalotUniqueText <- variablesWithalotUniqueText[!variablesWithalotUniqueText %in% c("last_credit_pull_d", "earliest_cr_line", "issue_d")]
 variablesWithOneValue       <- structureData$variable[which(structureData$uniqueValues == 1 )]
-variablesFromFeature        <- c("next_pymnt_d", "installment", "int_rate", "term", "recoveries", "collection_recovery_fee",  "last_pymnt_amnt", "grade", "term")
-variablesAnotherToDelete    <- c("loan_status","id", "member_id", "loan_amnt", "funded_amnt_inv", "out_prncp", "out_prncp_inv", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee" )
+variablesToRemove           <- c(variablesWithalotNA, variablesWithalotUniqueText, variablesWithOneValue)
+variablesFromFeature        <- c("sub_grade", "grade", "int_rate", "installment", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee")
+variablesAnotherToDelete    <- c("id", "member_id", "issue_id", "loan_status")
+
+dataReducedByVariables <- dataReduced %>% select(-variablesToRemove)
 
 
-variables <- names(data)
-variablesToGet <- variables[!variables %in% c(variablesWithalotNA, variablesWithalotUniqueText,  variablesFromFeature, variablesAnotherToDelete, variablesWithOneValue)]  
-dataWork <- data[, variablesToGet]
 
-dataWork <- dataWork %>%
-  mutate(funded_loan_date = convert_date_from_month_year(issue_d),
-         earliest_cr_line_date = convert_date_from_month_year(earliest_cr_line),
-         last_credit_pull_date = convert_date_from_month_year(last_credit_pull_d)) %>%
-  select(-c("issue_d","earliest_cr_line", "last_credit_pull_d"))
 
 dataWork <- changeCharacter2FactorVariableWithLackGroup(data = dataWork,  typeOfLack = "n/a" )
 
