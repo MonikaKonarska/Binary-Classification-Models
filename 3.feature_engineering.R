62d8253c1cc2afb34d61c0516b93fc3c18e4c0b2
monikakkk <monika.konarska63@gmail.com>
2019-07-07 21:16:31 +0200

    remove script
    the code of this old script I add to new script 3.feature_engineering.R

--- 3.feature_engineering.R
+++ 3.feature_engineering.R
@@ -8,23 +8,28 @@ source("functions.R")
 dataPath <- file.path(getwd(), "data")
 load(file.path(dataPath, "dataToModeling.Rdata"))
 
-# for test decision to reduce observations
-dataTrain <- dataTrain %>% filter(funded_loan_date >= '2013-01-01')
-dataTrain$month <- substr(dataTrain$funded_loan_date, start = 1, stop = 7)
+
+
+# Feature engineering / feature selection only on training data + EDA tylko z danych dataTrain !
+# a pozniej future selection - juz po eda
+ xxxTmp <- changeCharacter2FactorVariableWithLackGroup(data = dataReducedByVariables,  typeOfLack = "n/a" )
+
+ xxxTmp$time_credit_history_year <- round((dataReducedByVariables$funded_loan_date- dataReducedByVariables$earliest_cr_line_date)/365, 2)
+
+
+
+# feature selection: https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
+# zamiana z character na factor
+# grupowanie zmiennych ciaglych i dyskretnych
+
+
+####################
+
+
+dataTrain$month <- substr(dataTrain$funded_loan_date, start = 1, stop = 7) # moze nie month ale busket ?
 dataTrain$month <- factor(dataTrain$month, levels = sort(unique(dataTrain$month)))
+#dataTrain <- dataTrain %>% select(-c("mths_since_last_record", "mths_since_last_major_derog", "mths_since_last_delinq"))
 
-dataTrain <- dataTrain %>% select(-c("mths_since_last_record", "mths_since_last_major_derog", "mths_since_last_delinq"))
-
-
-attributesOfVariables <- list()
-
-for (var in names(dataTrain)) {
-  if(is.factor(dataTrain[[var]])) {
-    attributesOfVariables[[var]] <- table(dataTrain[[var]])
-  } else {
-    attributesOfVariables[[var]] <- summary(dataTrain[[var]])
-    }
-}
 
 # in all samples (train, test, vaid, out of time) the attributes: NONE, OTHER, ANY have got a very small percentage so I decide to remove it from all data
 dataTrain <- dataTrain %>% filter(!home_ownership %in% c('ANY', 'NONE', 'OTHER'))
@@ -37,15 +42,14 @@ dataTrain$emp_length <- ifelse(as.character(dataTrain$emp_length) == 'n/a', 'LAC
 dataTrain$emp_length <- factor(dataTrain$emp_length, levels = sort(unique(dataTrain$emp_length)))
 
 
-variablesWithNA <- c()
+dataWork <- changeCharacter2FactorVariableWithLackGroup(data = dataWork,  typeOfLack = "n/a" )
 
-for (i in names(dataTrain)) {
-  if(sum(is.na(dataTrain[i])) > 0) {
-    print(i)
-    dataTrain       <- dataTrain[!is.na(dataTrain[[i]]), ]
-    variablesWithNA <- c(i, variablesWithNA)
-  }
-}  
+# find to large percent of attribute in factor variables
+factorColumns <- names(select_if(.tbl =  dataWork, is.factor))
+attributesOfVariables <- sapply(factorColumns, function(x) prop.table(table(dataWork[[x]])))
+
+variablesWithLargeAttribute <- c("verification_status_joint", "application_type", "pymnt_plan")
+dataWork <- dataWork %>% select(-one_of(variablesWithLargeAttribute))
 
 
 ##  Exploratory Data Analysis
@@ -65,4 +69,11 @@ dataTrain %>%
 densityPlots <- lapply(numericVariables, function(x) createPlotsForContinousVariables(variable_name = x, data = dataTrain, type_of_plots = "density", groupBy = "month"))
 boxplotPlots <- lapply(numericVariables, function(x) createPlotsForContinousVariables(variable_name = x, data = dataTrain, type_of_plots = "boxplot", groupBy = "month"))
 histogramPlots <- lapply(numericVariables, function(x) createPlotsForContinousVariables(variable_name = x, data = dataTrain, type_of_plots = "histogram", groupBy = "month"))
+
+
+
+# liczba zapytan/ kliencow w miesiacach
+# liczba deafoultow w miesiacach
+
+
 
