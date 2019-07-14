6acd89b116d83a300899af0cfd1e754fb0c0d9e3
monikakkk <monika.konarska63@gmail.com>
2019-07-14 21:52:21 +0200

    update list of variables "from future"

--- 1.data_cleaning.R
+++ 1.data_cleaning.R
@@ -17,8 +17,8 @@ variablesWithalotNA         <- structureData$variable[which(structureData$p_numb
 variablesWithalotUniqueText <- structureData$variable[ which(structureData$type == 'character' & structureData$uniqueValues > maxUniqueValues)]
 variablesWithOneValue       <- structureData$variable[which(structureData$uniqueValues == 1 )]
 variablesToRemove           <- c(variablesWithalotNA, variablesWithalotUniqueText, variablesWithOneValue)
-variablesFromFeature        <- c("sub_grade", "grade", "int_rate", "installment", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee")
-variablesAnotherToDelete    <- c("id", "member_id", "issue_id", "loan_status")
+variablesFromFeature        <- c("sub_grade", "grade", "int_rate", "installment", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_pymnt_amnt", "initial_list_status")
+variablesAnotherToDelete    <- c("id", "member_id", "issue_d", "loan_status", "funded_amnt_inv", "term", "verification_status", "funded_amnt", "group", "last_credit_pull_d", "collections_12_mths_ex_med", "acc_now_delinq")
 
 dataToTranTestValid_cleaned <- dataToTranTestValid %>%
   select(-variablesToRemove)
