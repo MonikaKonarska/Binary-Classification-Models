19ecfd134adca40f5d54df41e60b2f127ab1a56d
monikakkk <monika.konarska63@gmail.com>
2019-07-14 21:50:04 +0200

    create woe plots for variables and iv plot in time

--- 4.feature_selection.R
+++ 4.feature_selection.R
@@ -16,3 +16,49 @@ iv_for_all_variables_plot <- ggplot(dataToPlotingIV, aes(x = Variable, y = IV)) 
   theme_bw() +
   theme(plot.title = element_text(size = 10)) +
   theme(axis.text.x = element_text(angle = 90))
+
+
+
+
+
+prospectiveVariables <- names(information_table$Tables)
+plotsOfWoe           <- list()
+plotsOfIvInTime      <- list()
+
+variable<-'loan_amnt'
+for (variable in prospectiveVariables) {
+  
+  tableWithWoes <- information_table$Tables
+  IValue <- information_table$Summary[which(information_table$Summary$Variable == variable), "IV"]
+  
+  woe_plot <-  ggplot(tableWithWoes[[variable]],  aes_string(x = variable, y = 'WOE'))+
+    geom_bar(stat = 'identity', fill = "darkblue")+
+    theme(axis.text.x = element_text(angle = 90))+
+    ggtitle(label = "Weight of Evidence (WOE)", subtitle = paste0("Variable: ", variable, "\n", "Information value:", round(IValue,4)))
+  
+  plotsOfWoe[[variable]] <- woe_plot
+  
+  
+  quarters <- as.character(unique(dataTrainWithNewFeatures$quarter))
+  woeInTime <- data.frame(quarter = quarters, iv = NA, stringsAsFactors = FALSE)
+  
+  for (q in quarters) {
+    
+    data <- dataTrainWithNewFeatures %>%
+      select_('quarter', variable, 'target') %>%
+      filter(quarter == q) %>%
+      select(-c(quarter))
+    
+    informationTableForVariable <- create_infotables(data, y = "target", bins = 6)
+    woeInTime[which(woeInTime[['quarter']] == q), 'iv'] <- informationTableForVariable$Summary$IV
+  }
+  
+  plotIvInTime <- ggplot(data = woeInTime, aes(x = quarter, y = iv))+
+    geom_bar(stat = 'identity', fill = "green")+
+    theme(axis.text.x = element_text(angle = 90))+
+    ggtitle(label = "Information value for each quarter in train dataset",
+            subtitle = paste0("Variable: ", variable))
+  
+  plotsOfIvInTime[[variable]] <- plotIvInTime
+  
+}
