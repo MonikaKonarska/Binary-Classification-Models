af1d2ea591f8498cf61c7b62a79c262429158e64
monikakkk <monika.konarska63@gmail.com>
2019-08-06 20:33:35 +0200

    define in vector continuous variable with not monotonic values of woe

--- 4.feature_selection.R
+++ 4.feature_selection.R
@@ -13,11 +13,10 @@ results <- calculate_information_value_for_variables(data = dataTrainWithNewFeat
                                                      create_plots_woe = TRUE,
                                                      name_of_time_variable_to_iv = "quarter")
 
-variable_selection_step_iv       <- results$information_table$Summary[which(results$information_table$Summary$IV > 0.02),]
+variable_selection_step_iv       <- results$information_table$Summary[which(results$information_table$Summary$IV > 0.02), ]
 names_variable_selection_step_iv <- variable_selection_step_iv$Variable
 
 dataTrainWithSelectionIvFeatures <- dataTrainWithNewFeatures %>% select(c(names_variable_selection_step_iv, "target", "quarter"))
-
 # Stability information value in quarters for each variable
 results_for_variable_selection <- calculate_information_value_for_variables(data = dataTrainWithSelectionIvFeatures,
                                                                             name_of_dependent_variable = "target",
@@ -26,8 +25,9 @@ results_for_variable_selection <- calculate_information_value_for_variables(data
                                                                             name_of_time_variable_to_iv = "quarter")
 
 save(results_for_variable_selection, file = file.path(folderToSavePlots, "results_for_variable_selection.RData"))
+continuous_variable_with_not_monotonic <- c("pub_rec", "loan_amnt", "emp_length", "open_acc", "revol_bal")
+variables_joining_woe_category         <- c("inq_last_6_mths_cat", "mths_since_last_delinq", "mths_since_last_major_derog")
 
-continuous_variable_with_not_monotonic <- c("pub_rec", "loan_amnt", "emp_length", "open_acc", "revol_bal", "")
-variables_joining_woe_category <- c("inq_last_6_mths_cat", "", "mths_since_last_delinq", "mths_since_last_major_derog")
-
-
+# optimal number of bins for variables
+numeric_variables <- dataTrainWithNewFeatures %>% select_if(is.numeric) %>% names()
+numeric_variables <- numeric_variables[which(!numeric_variables %in% c('target'))]
