82693513642fd19f7912b11f229167d501c0b3ce
monikakkk <monika.konarska63@gmail.com>
2019-08-19 21:35:46 +0200

    add main script

---
+++ main.R
@@ -0,0 +1,13 @@
+source("0a.importData.R")
+source("0b.choose_time_series_to_modeling.R")
+source("1.data_cleaning.R")
+source("2.sampling.R")
+source("3.feature_engineering.R")
+source("4.feature_selection.R")
+
+data <- importData()
+chooseTimeSeriesToModeling()
+dataCleaning()
+samplingTrainTestValid()
+dataTrainWithNewFeatures <- featurEngineering()
+
