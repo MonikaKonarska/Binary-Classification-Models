4193faf287e2fc8dc08c0e91dc5315619cc24aa9
monikakkk <monika.konarska63@gmail.com>
2019-08-19 21:37:06 +0200

    modify script to function dataCleaning

--- 1.data_cleaning.R
+++ 1.data_cleaning.R
@@ -4,30 +4,25 @@ library(reshape)
 library(scales)
 source("functions.R")
 
-dataPath <- file.path(getwd(), "data")
-load(file.path(dataPath, "dataToTranTestValid.Rdata"))
+dataCleaning <- function() {
+  
+  #dataPath <- file.path(getwd(), "data")
+  load(file.path(dataPath, "dataToTranTestValid.Rdata"))
 
-
-# Data cleaning
-structureData <- describe_variables(dataToTranTestValid)
-levelOfNA <- 0.80
-maxUniqueValues <- 30
-
-variablesWithalotNA         <- structureData$variable[which(structureData$p_numberOfNa >= levelOfNA )]
-variablesWithalotUniqueText <- structureData$variable[ which(structureData$type == 'character' & structureData$uniqueValues > maxUniqueValues)]
-variablesWithOneValue       <- structureData$variable[which(structureData$uniqueValues == 1 )]
-variablesToRemove           <- c(variablesWithalotNA, variablesWithalotUniqueText, variablesWithOneValue)
-variablesFromFeature        <- c("sub_grade", "grade", "int_rate", "installment", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_pymnt_amnt", "initial_list_status")
-variablesAnotherToDelete    <- c("id", "member_id", "issue_d", "loan_status", "funded_amnt_inv", "term", "verification_status", "funded_amnt", "group", "last_credit_pull_d", "collections_12_mths_ex_med", "acc_now_delinq")
-
-dataToTranTestValid_cleaned <- dataToTranTestValid %>%
-  select(-variablesToRemove)
-
-dataToTranTestValid_cleaned <- dataToTranTestValid_cleaned[which(!dataToTranTestValid_cleaned$home_ownership == "ANY"), ]
-
-
-dataTrain  <- dataToTranTestValid_cleaned %>% filter(group == "train")
-dataTest   <- dataToTranTestValid_cleaned %>% filter(group == "test")
-dataValid  <- dataToTranTestValid_cleaned %>% filter(group == "valid")
-
-save(dataTrain, dataTest, dataValid, file = file.path(dataPath, "dataToModeling.RData"))
+  structureData <- describe_variables(dataToTranTestValid)
+  levelOfNA <- 0.80
+  maxUniqueValues <- 30
+  
+  variablesWithalotNA         <- structureData$variable[which(structureData$p_numberOfNa >= levelOfNA )]
+  variablesWithalotUniqueText <- structureData$variable[ which(structureData$type == 'character' & structureData$uniqueValues > maxUniqueValues)]
+  variablesWithOneValue       <- structureData$variable[which(structureData$uniqueValues == 1 )]
+  variablesToRemove           <- c(variablesWithalotNA, variablesWithalotUniqueText, variablesWithOneValue)
+  variablesFromFeature        <<- c("sub_grade", "grade", "int_rate", "installment", "total_pymnt", "total_pymnt_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_pymnt_amnt", "initial_list_status")
+  variablesAnotherToDelete    <<- c("id", "member_id", "issue_d", "loan_status", "funded_amnt_inv", "term", "verification_status", "funded_amnt", "group", "last_credit_pull_d", "collections_12_mths_ex_med", "acc_now_delinq")
+  
+  dataToTranTestValid_cleaned <- dataToTranTestValid %>%
+    select(-variablesToRemove)
+  
+  dataToTranTestValid_cleaned <- dataToTranTestValid_cleaned[which(!dataToTranTestValid_cleaned$home_ownership == "ANY"), ]
+  save(dataToTranTestValid_cleaned, file = file.path(dataPath, "dataToTranTestValid_cleaned.RData"))
+}
