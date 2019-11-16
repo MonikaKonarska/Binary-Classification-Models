6ae9b0b19357a323f3dca4ce902393a1eefe6d47
monikakkk <monika.konarska63@gmail.com>
2019-11-16 16:14:22 +0100

    add script Rmd

--- main.R
+++ main.R
@@ -3,11 +3,12 @@ source("0b.choose_time_series_to_modeling.R")
 source("1.data_cleaning.R")
 source("2.sampling.R")
 source("3.feature_engineering.R")
-source("4.feature_selection.R")
+#source("4.feature_selection.R")
 
 data <- importData()
 chooseTimeSeriesToModeling()
 dataCleaning()
 samplingTrainTestValid()
-dataTrainWithNewFeatures <- featurEngineering()
+dataTrainWithNewFeatures <<- featurEngineering()
+
 
