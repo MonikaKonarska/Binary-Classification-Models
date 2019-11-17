d57f3e16add12219326f3a11f99ed9712379f46b
monikakkk <monika.konarska63@gmail.com>
2019-11-17 14:05:32 +0100

    add new features if_delinq_in2yrs, if_delinq_in2yrs, if_delinq_in2yrs, if_delinq_in2yrs, if_delinq_in2yrs, if_delinq_in2yrs

--- 3.feature_engineering.R
+++ 3.feature_engineering.R
@@ -9,22 +9,21 @@ featurEngineering <- function() {
   dataTrain <- dataTrain[, which(!colnames(dataTrain) %in% c(variablesFromFeature, variablesAnotherToDelete))]
   
   dataTrainWithNewFeatures <- dataTrain %>%
-    mutate(delinq_2yrs_binary = factor(case_when(delinq_2yrs == 0 ~ 1,
-                                                 TRUE ~0), levels = c(0,1)),
-           deninq_in_last_year_cat = case_when(is.na(mths_since_last_delinq) ~ "LACK",
+    mutate(if_delinq_in2yrs = factor(case_when(delinq_2yrs == 0 ~ 0, TRUE ~ 1), levels = c(0, 1)),
+           
+           if_delinq_in_last_year = factor(case_when(is.na(mths_since_last_delinq) ~ "LACK",
                                                mths_since_last_delinq <= 12 ~ "1",
-                                               TRUE ~ "0"),
-           inq_last_6mths_cat = factor(case_when(inq_last_6mths == 0 ~ "0",
-                                                 inq_last_6mths == 1 ~ "1",
-                                                 inq_last_6mths >= 2 ~ "2+"), levels = c("0", "1", "2+")),
-           inq_last_6mths_catMore = factor(case_when(inq_last_6mths >= 4 ~ "4+",
+                                               TRUE ~ "0")),
+           if_delinq_ever = factor(case_when(is.na(mths_since_last_delinq) ~ 0, TRUE ~ 1)),
+           if_inq_in_last_6moths = factor(case_when(inq_last_6mths == "0" ~ 0, TRUE ~ 1)),
+           number_inq_in_last_6mths = factor(case_when(inq_last_6mths == 0 ~ "0",
+                                                       inq_last_6mths == 1 ~ "1",
+                                                       inq_last_6mths >= 2 ~ "2+"), levels = c("0", "1", "2+")),
+           inq_last_6mths_grouped = factor(case_when(inq_last_6mths >= 4 ~ "4+",
                                                       TRUE ~ as.character(inq_last_6mths)), levels = c("0","1","2","3","4+")),
-           if_inq_last_6mths_cat = factor(case_when(inq_last_6mths == 0 ~ "0",
-                                                   inq_last_6mths >= 1 ~ "2+"), levels = c("0", "2+")),
-           mths_since_last_delinq_binary = factor(case_when(mths_since_last_delinq >= 1 ~ 1,
-                                                            TRUE ~ 0), levels = c(0,1)),
-           pub_rec_value_binary = factor(case_when(pub_rec == 0 ~ 0,
-                                                   TRUE ~1), levels = c(0,1)),
+           if_ever_pub_rec = factor(case_when(pub_rec == 0 ~ 0, TRUE ~1), levels = c(0,1)),
+           if_purpose_debt_consolidation = factor(case_when(purpose == 'debt_consolidation' ~ 1, TRUE ~ 0), levels = c(0,1)),
+           if_employment_more_10years = factor(case_when(emp_length == '10+ years' ~ 1, TRUE ~ 0)),
            month = substr(x = funded_loan_date, start = 1, stop = 7),
            month = factor(month, levels = sort(unique(month))),
            number_of_quarter = lubridate::quarter(funded_loan_date),
@@ -33,7 +32,7 @@ featurEngineering <- function() {
     select(-c(funded_loan_date, earliest_cr_line_date, last_credit_pull_date, number_of_quarter, year_of_funded_loan))
   
   dataTrainWithNewFeatures <- changeCharacter2FactorVariableWithLackGroup(data = dataTrainWithNewFeatures,
-                                                                          typeOfLack = "n/a")
+                                                                          typeOfLack = "LACK")
   return(dataTrainWithNewFeatures)
 }
 
