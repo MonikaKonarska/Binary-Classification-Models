de233d0a8225d69d80ff6233d23f79662fb41ef3
monikakkk <monika.konarska63@gmail.com>
2019-11-18 22:30:58 +0100

    add function to remove outliers from dataset

---
+++ outliers_detection_and_correlations.R
@@ -0,0 +1,19 @@
+# Outliers detection and correlations 
+source("functions.R")
+
+removeOutliers <- function(data_set) {
+  
+  numeric_columns <- names(data_set)[map_lgl(data_set, is.numeric)]
+  removedOtliersFromTrainingData <- integer(length = length(numeric_columns))
+  names(removedOtliersFromTrainingData) <- numeric_columns
+  
+  for (variable in numeric_columns) {
+    outliers <- !detect_not_outliers(data_set, column_name = variable)
+    number_otliers <- sum(outliers)
+    removedOtliersFromTrainingData[[variable]] <- number_otliers
+    data_set <- data_set[!outliers, ]
+  }
+  print(removedOtliersFromTrainingData)
+  return(data_set)
+}
+
