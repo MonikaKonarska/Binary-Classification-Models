2df216fc507555025f33a8851a43879e45b8000e
monikakkk <monika.konarska63@gmail.com>
2019-12-01 13:36:55 +0100

    define correct the definition of target

--- 0b.choose_time_series_to_modeling.R
+++ 0b.choose_time_series_to_modeling.R
@@ -19,14 +19,16 @@ chooseTimeSeriesToModeling <- function() {
            earliest_cr_line_date = convert_date_from_month_year(earliest_cr_line),
            last_credit_pull_date = convert_date_from_month_year(last_credit_pull_d)) 
   
-  data$target <- factor(case_when(data$loan_status == "Fully Paid"  ~ 0,
-                                  data$loan_status == "Charged Off" ~ 1,
-                                  data$loan_status == "Default Does not meet the credit policy. Status:Charged Off" ~ 1), levels = c(0, 1))
+  data$target <- case_when(data$loan_status == "Fully Paid"  ~ '0',
+                                  data$loan_status == "Charged Off" ~ '1',
+                                  data$loan_status == "Default Does not meet the credit policy. Status:Charged Off" ~ '1',
+                           TRUE ~ as.character(data$loan_status))
   
-  data <- data[which(!is.na(data$target)), ]
+  data <- data[which(data$target %in% c("0", "1")), ]
+  data$target <- as.factor(data$target)
   
   plotTargetInAllData <- data %>%
-    select(c("target", "funded_loan_date")) %>%
+    dplyr::select(target, funded_loan_date) %>%
     group_by(funded_loan_date, target) %>%
     dplyr::summarise( N = n()) %>%
     ggplot(aes(x = funded_loan_date, y = N, fill=target)) +
@@ -36,7 +38,7 @@ chooseTimeSeriesToModeling <- function() {
     theme(plot.title = element_text(hjust = 0.5))
 
   defaultRates <- data %>%
-    select(c("target", "funded_loan_date")) %>%
+    dplyr::select(c("target", "funded_loan_date")) %>%
     group_by(funded_loan_date, target) %>%
     dplyr::summarise(N = n()) %>%
     melt(id = c("funded_loan_date", "target")) %>%
@@ -50,8 +52,8 @@ chooseTimeSeriesToModeling <- function() {
            badRate = bads/goods,
            N = goods + bads)
   
-  numberOfMinObsInMonth      <- 1500
-  numberOfMinDefaultsInMonth <- 500
+  numberOfMinObsInMonth      <- 1000
+  numberOfMinDefaultsInMonth <- 100
     
   divTrain <- 0.6
   divTest  <- 0.2
@@ -59,8 +61,8 @@ chooseTimeSeriesToModeling <- function() {
   group_by_time <- 'month'
   
   modelingTimeInterval <- defaultRates %>%
-    filter(N > numberOfMinObsInMonth & bads >= numberOfMinDefaultsInMonth) %>%
-    select(funded_loan_date) %>%
+    dplyr::filter(N > numberOfMinObsInMonth & bads >= numberOfMinDefaultsInMonth) %>%
+    dplyr::select(funded_loan_date) %>%
    dplyr::summarise(minDate = min(funded_loan_date),
                     maxDate = max(funded_loan_date))
   
@@ -70,11 +72,11 @@ chooseTimeSeriesToModeling <- function() {
     scale_y_continuous(limits = c(0.15, 0.35), labels = scales::percent)+
     theme(plot.title = element_text(hjust = 0.5))
   
-  minDateInDataSet <- as.Date('2012-01-01')
+  minDateInDataSet <- as.Date('2012-10-01')
   maxDateInDataSet <- as.Date('2014-12-31')
   
   plotBadRateSelectedData <- defaultRates %>% 
-    filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet) %>%
+    dplyr::filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet) %>%
     ggplot(aes(x = funded_loan_date, y = badRate))+
     geom_line() +
     labs(title = "Bad rate - selected data") +
@@ -83,9 +85,9 @@ chooseTimeSeriesToModeling <- function() {
           plot.subtitle = element_text(size = 9))
   
   plotTargetInSelectedData <- data %>%
-    select(c("target", "funded_loan_date")) %>%
-    filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet) %>%
-    group_by(funded_loan_date, target) %>%
+    dplyr::select(c("target", "funded_loan_date")) %>%
+    dplyr::filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet) %>%
+    dplyr::group_by(funded_loan_date, target) %>%
     dplyr::summarise( N = n()) %>%
     ggplot(aes(x = funded_loan_date, y = N, fill = target)) +
     geom_bar(stat = 'identity', position = position_dodge())+
@@ -108,10 +110,10 @@ chooseTimeSeriesToModeling <- function() {
   save_plot_jpg(path = folderToSavePlots, plot = plotTargetsInformation, nameOfPlot = "plotTargetsInformation")
   
   dataToTranTestValid <- data %>%
-    filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet %m-% years(1))
+    dplyr::filter(funded_loan_date >= minDateInDataSet & funded_loan_date <= maxDateInDataSet %m-% years(1))
   
   dataNew <- data %>%
-    filter(funded_loan_date > minDateInDataSet %m+% years(1) & funded_loan_date <= maxDateInDataSet) 
+    dplyr::filter(funded_loan_date > minDateInDataSet %m+% years(1) & funded_loan_date <= maxDateInDataSet) 
   
   save(dataToTranTestValid, file = file.path(dataPath, "dataToTranTestValid.Rdata"))
   save(dataNew, file = file.path(dataPath, "dataNew.Rdata"))
