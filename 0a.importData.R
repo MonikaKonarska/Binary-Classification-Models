e256ccbd150c9a9f25b51511947eea54dfccfb8d
monikakkk <monika.konarska63@gmail.com>
2019-12-30 21:42:39 +0100

    move definition of file path to main.R script

--- 0a.importData.R
+++ 0a.importData.R
@@ -1,8 +1,6 @@
 library(data.table)
 
 importData <- function() {
-  saveResultsPath <<- file.path(getwd(), "dataResults")
-  dataPath        <<- file.path(getwd(), "data")
   data            <- fread(file.path(dataPath, "loan.csv"))
   data            <- setDF(data)
 }
