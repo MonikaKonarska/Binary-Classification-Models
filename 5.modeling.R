32b4e1673d014cad42fe43f5cd697573a875d4c9
monikakkk <monika.konarska63@gmail.com>
2019-12-31 18:10:16 +0100

    remove files

--- 5.modeling.R
+++
@@ -1,9 +0,0 @@
-source("functions_to_feature_selection.R")
-
-
-
-
-model <- glm(formula = target~dtiWoe+annual_incWoe+revol_utilWoe+total_rev_hi_limWoe+home_ownershipWoe,
-             data = dataTrainWithSelectionIvFeaturesWoe,
-             family = binomial(link='logit'))
-
